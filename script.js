6336dd63a72f990f340e5027dbef4d9ca669c739
Aryan Kumar <aimpendance@gmail.com>
2026-05-30 10:54:48 -0600

    Javascript in Progress and SCSS finished

---
+++ script.js
@@ -0,0 +1,25 @@
+// let count = 5;
+// console.log(count);
+// console.log(count++)
+// console.log(count)
+// console.log(++count)
+
+// let count = 5;
+// console.log(count);
+// console.log(count--)
+// console.log(count)
+// console.log(--count)
+
+// Function Hoisting - using the function before making it , only can be used if its declared as this not as
+// a function within a var const or let in that case the let goes in the temporal dead zone and gives u 
+// a reference error 
+// hello();
+
+// function hello(){
+//     console.log("function is running")
+// }
+
+// // Var hoisting - should give a ref error since js treats it as vara ; cloga ; a=10
+// console.log(a); // undefined
+
+// var a = 10;
