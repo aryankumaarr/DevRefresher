8e05f89b642e0e2e8caf882c4135a94980af656d
Aryan Kumar <aimpendance@gmail.com>
2026-06-04 10:30:47 -0600

    More Questions

--- script.js
+++ script.js
@@ -11,8 +11,8 @@
 // console.log(--count)
 
 // Function Hoisting - using the function before making it , only can be used if its declared as this not as
-// a function within a var const or let in that case the let goes in the temporal dead zone and gives u 
-// a reference error 
+// a function within a var const or let in that case the let goes in the temporal dead zone and gives u
+// a reference error
 // hello();
 
 // function hello(){
@@ -23,7 +23,6 @@
 // console.log(a); // undefined
 
 // var a = 10;
-
 
 // // Conditional Operators
 
@@ -45,3 +44,85 @@
 //         console.log("default");
 // }
 
+// // Loops
+// // for loops
+// for(i=1;i<5;i++){
+//     console.log(i)
+// }
+// for (i=22;i<33;i++){
+//     console.log(i);
+// }
+// for(i=30;i>5;i--){
+//     console.log(i);
+// }
+// // Practicing some auth concepts using while - verifying password using ltd attempts
+// let attempts = 0;
+// let accountStatus = false;
+// let key = "Aryan"
+
+// let password = prompt("Enter a password").trim()
+// attempts++;
+
+// if(password === key) accountStatus = true , console.log("Account opened");
+
+// while (password !== key) {
+//   if (attempts == 3 && password !== key) {
+//     console.error("Locked out");
+//     break;
+//   }
+//   password = prompt("Enter a password");
+//   if(password === key) accountStatus = true , console.log("Account opened");
+//   attempts++;
+// }
+
+//  Loop Practice Question 2
+// 12. Ask user for words until they type “stop”. Count how many times they typed “yes”
+// Loop until "stop" is typed. Count "yes".
+
+// let words = prompt("Type in some words").trim().toLowerCase();
+// let yesCount = 0;
+
+// while (words !== "stop" ) {
+//   if (words === "yes") yesCount++;
+//   words = prompt("Type in some words").trim().toLowerCase();
+// }
+
+// console.log("stopped loop");
+// console.log(`yes used ${yesCount} amount of times`);
+
+// // Print only first 3 numbers from 1 to 20 using a loop and add break after 3 odd prints.
+// let oddCounter = 0;
+// for (i = 0; i <= 20; i++) {
+//   if (oddCounter === 3) break;
+//   if (i % 2 != 0) {
+//     console.log(i);
+//     oddCounter++;
+//   }
+// }
+
+// ask user for 5 numbers count how many positives , use loop conition and a counter
+// let positiveCounter = 0;
+
+// for(i=0;i<5;i++){1
+//     let num = +prompt("Enter a number")
+//     if(num>=0) positiveCounter++;
+// }
+//  console.log(`Number of Positives ${positiveCounter}`);
+
+// Atm Sim: Allow 3 withdrawls , starting balance $1000 dollars, ask for withdrawing 3 times
+// if Enough BAlance deduct if not log insufficient balance
+
+// let balance = 1000;
+// let withdrawlCount = 0;
+// for (let i = 0; i < 3; i++) {
+//   let withdrawingAmount = +prompt("Enter Amount");
+//   if (balance === 0 || balance < withdrawingAmount) {
+//     console.error("Insufficient Funds");
+//     break;
+//   }
+//   if (balance >= withdrawingAmount) {
+//     balance-= withdrawingAmount;
+//     withdrawlCount++;
+//   }
+// }
+// NAN , deposis and other things not handled yet 
