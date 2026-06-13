d9a002ffe4facf8ced3858a9cdb3e45aedd34f1c
Aryan Kumar <aimpendance@gmail.com>
2026-06-13 15:23:58 -0600

    DOM concepts

---
+++ script_DOM.js
@@ -0,0 +1,40 @@
+// DOM - Document Object Model
+// Accessing elements , query selector , queryselectorall , get element
+
+// var navElement1 = document.querySelector('#navitem1')
+// navElement1.innerHTML = 'Hello' //setting or changing value inside element
+
+// querySelectorAll
+// var allNavElements = document.querySelectorAll('.navitems');
+
+// allNavElements.forEach(item=>{
+//     item.textContent = "Hello";
+// })
+
+// remember to use foreach if u refer to multiple items with the same classname or something
+
+// getElemnent by id or class
+
+// var btn = document.getElementsByClassName('button_1')[0]; 
+// // now instead of the loop u can also use array index like this to select something becasue js will always return
+// // a collection if ure using methods that have a multiple name like queryselectorALL or getelemntbyClassName
+
+// // incase of getelemnt byid or queryselector u can just set as needed without loop or array index
+// btn.style.color = "red";
+
+// adding event listener
+// btn.addEventListener('click',function(){
+//     console.log("Hello");
+// })
+
+// -- CREATING ELEMENTS -- //
+// var h1 = document.createElement('h1');
+// console.log(h1)
+
+// -- APPEND ELEMENTS -- //
+
+var greet = document.createElement('h1')
+greet.innerHTML='hello'
+
+var main = document.querySelector('#main')
+main.appendChild(greet)
